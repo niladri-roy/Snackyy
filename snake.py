f4c672c2edca091223f9d71e6f5704d7196ce5f1
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2022-07-28 15:18:55 +0530

    snakyy

--- snake.py
+++ snake.py
@@ -36,7 +36,65 @@ def make_snake(snake_block, list_snake):
         pygame.draw.rect(add_caption, colour_3, [x[0],x[1],snake_block])
 
 
-def display_msg(msg,color)
+def display_msg(msg,color):
     mssg = display_style.render(msg, True, color)
-    
+    add_caption.blit(mssg,[box_len/6, box_height/3])
+
+def game_start():
+    game_over = False
+    game_close = False
+
+    value_x1 = box_len/2
+    value_y1 = box_height/2
+
+    new_x1 = 0
+    new_y1 = 0
+
+    list_snake = []
+    snake_len = 1
+
+    foodx_pos = round(random.randrange(0, box_len-snake_block))
+    foody_pos = round(random.randrange(0, box_height-snake_block))
+
+    while not game_over:
+
+        while game_close == True:
+
+            add_caption.fill(colour_6)
+            display_msg("You Lost! Wanna play again Press C else Press Q", colour_4)
+            final_score(snake_len-1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+
+                    if event.ky == pygame.K_c:
+                        game_start()
+        
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                game_over = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    new_x1 = -snake_block
+                    new_y1 = 0
+
+                elif event.key == pygame.K_RIGHT:
+                    new_x1 = snake_block
+                    
+
+
+            
+
+
+
+
+
      
